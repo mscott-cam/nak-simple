5d7e9db849cc2874f9e25cfcd668b9ef2afa3a07
Scott Cameron <Scott.Cameron@US-861.local>
2021-12-22 17:14:48 -0600

    styled comps install

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from "styled-components/native";
+import { Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <AppView>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    </AppView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppView = styled.View`
+  flex: 1;
+`;
