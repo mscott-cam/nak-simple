fb6ee8167793fc50d6d5379bc5f5df8ea4f43a6e
Scott Cameron <mscottcam@gmail.com>
2021-12-29 11:48:13 -0600

    tests action reminder text

--- src/components/action-reminder/ActionReminder.test.tsx
+++ src/components/action-reminder/ActionReminder.test.tsx
@@ -1,7 +1,9 @@
 import { render } from '@testing-library/react-native'
 import { ActionReminder } from './ActionReminder'
 
-test('', () => {
-  const { container } = render(<ActionReminder />)
+test('if action reminder displays properly', () => {
+  const text = 'test action reminder text'
+  const { container, queryByText } = render(<ActionReminder text={text} />)
   expect(container).toBeDefined()
+  expect(queryByText(text)).not.toBeNull()
 })
